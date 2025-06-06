ca47ad5b857d2afd9eb85656839688866172acbe
IlyaTrofyanin <ilya.trofyanin@mail.ru>
2025-06-06 10:43:36 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+def my_function(x, y):
+    """Add two numbers."""
+    return x + y
+
+
+def main():
+    """Main function."""
+    x = 1
+    y = 2
+    result = my_function(x, y)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
